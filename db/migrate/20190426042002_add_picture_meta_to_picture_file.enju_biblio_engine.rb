e7c76e929dc7f3160b609f63fcf7c298cfe80ed3
Kosuke Tanabe <nabeta@fastmail.fm>
2019-05-05 15:03:25 +0900

    update migration files

--- db/migrate/20190426042002_add_picture_meta_to_picture_file.enju_biblio_engine.rb
+++
@@ -1,6 +0,0 @@
-# This migration comes from enju_biblio_engine (originally 20120413072700)
-class AddPictureMetaToPictureFile < ActiveRecord::Migration[5.2]
-  def change
-    add_column :picture_files, :picture_meta, :text
-  end
-end
