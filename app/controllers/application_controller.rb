dbfd2865e824c58cab88b2fa803c0e6717954a6c
Kosuke Tanabe <nabeta@fastmail.fm>
2020-01-13 02:08:32 +0900

    update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
-  include EnjuCirculation::Controller
-  include EnjuBiblio::Controller
-  include EnjuLibrary::Controller
-  include EnjuInventory::Controller
-  include EnjuSubject::Controller
-  include EnjuEvent::Controller
   include Pundit
   after_action :verify_authorized, unless: :devise_controller?
 end
