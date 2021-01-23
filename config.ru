07ed3063445f596ea342f80317137db752a10278
Kosuke Tanabe <nabeta@fastmail.fm>
2021-01-24 02:04:51 +0900

    update to Rails 6.1

--- config.ru
+++ config.ru
@@ -1,7 +1,6 @@
 # This file is used by Rack-based servers to start the application.
 
-require_relative 'config/environment'
-require 'rack/protection'
-use Rack::Protection, except: [:escaped_params, :json_csrf, :http_origin, :session_hijacking, :remote_token]
+require_relative "config/environment"
 
 run Rails.application
+Rails.application.load_server
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -8,18 +8,20 @@ Bundler.require(*Rails.groups)
 
 module EnjuLeafDocker
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    config.time_zone = "Asia/Tokyo"
     config.i18n.available_locales = [:en, :ja]
     config.i18n.enforce_available_locales = true
     config.active_job.queue_adapter = :resque
     config.i18n.default_locale = :ja
     config.i18n.fallbacks = [I18n.default_locale]
-    config.time_zone = 'Tokyo'
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
